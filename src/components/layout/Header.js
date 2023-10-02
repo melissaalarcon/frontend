d0bd71458a510393f891593969f86e9e01a4439d
Melissa Alarcón <mel.massai24@gmail.com>
2023-10-02 19:35:58 -0300

    test

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -4,6 +4,7 @@ const Header = (props) => {
     return (
         <header>
             <div className="holder">
+                <img></img>
                 <h1>Vial Gonett</h1>
             </div>
         </header>
